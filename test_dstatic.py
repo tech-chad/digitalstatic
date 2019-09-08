989a5f1fe61cedaa465044da3e8566dbf7224bdb
Chad Larson <techchad2@gmail.com>
2019-09-08 17:11:19 -0500

    Added some tests

--- test_dstatic.py
+++ test_dstatic.py
@@ -2,10 +2,12 @@
 
 import pytest
 from hecate import Runner
-
 import dstatic
 
-cmd = ["python3", "dstatic.py"]
+
+def dstatic_cmd(*args):
+    options = [a for a in args]
+    return ["python3", "dstatic.py"] + options
 
 
 @pytest.mark.parametrize("test_values, expected_results", [
@@ -20,9 +22,78 @@ def test_convert_delay_number_to_delay_time(test_values, expected_results):
 @pytest.mark.parametrize("test_key", ["q", "Q"])
 def test_dstatic_exit(test_key):
     """ Tests dstatic.py will exit on q or Q"""
-    with Runner(*cmd) as h:
+    with Runner(*dstatic_cmd()) as h:
         h.await_text("1")
         h.write(test_key)
         h.press("Enter")
         h.await_exit()
+
+
+@pytest.mark.parametrize("test_key", ["q", "Q"])
+def test_dstatic_exit_delay_set(test_key):
+    """
+    Tests dstatic.py will exit on q or Q
+    when setting delay time
+    """
+    with Runner(*dstatic_cmd("-d1")) as h:
+        h.await_text("1")
+        h.write(test_key)
+        h.press("Enter")
+        h.await_exit()
+
+
+@pytest.mark.parametrize("test_key", ["q", "Q"])
+def test_dstatic_exit_black_white_mode(test_key):
+    """
+    Tests dstatic.py will exit on q or Q
+    when using black and white mode
+    """
+    with Runner(*dstatic_cmd("-b")) as h:
+        h.await_text("1")
+        h.write(test_key)
+        h.press("Enter")
+        h.await_exit()
+
+
+@pytest.mark.parametrize("test_key", ["q", "Q"])
+def test_dstatic_exit_start_timer(test_key):
+    """
+    Tests dstatic.py will exit on q or Q
+    when using start timer
+    """
+    with Runner(*dstatic_cmd("-s1")) as h:
+        h.default_timeout = 2
+        # h.wait_interval = 3
+        h.await_text("1")
+        h.write(test_key)
+        h.press("Enter")
+        h.await_exit()
+
+
+@pytest.mark.parametrize("test_key", ["q", "Q"])
+def test_dstatic_exit_delay_set(test_key):
+    """
+    Tests dstatic.py will exit on q or Q
+    when using run timer.
+    """
+    with Runner(*dstatic_cmd("-r1")) as h:
+        h.await_text("1")
+        h.write(test_key)
+        h.press("Enter")
+        h.await_exit()
+
+
+def test_dstatic_run_timer_exit():
+    """ Test auto exit when using run timer. """
+    with Runner(*dstatic_cmd("-r2")) as h:
+        h.default_timeout = 3
+        h.await_text("1")
+        h.await_exit()
+
+
+def test_dstatic_start_timer():
+    """ Test start timer."""
+    with Runner(*dstatic_cmd("-s2")) as h:
+        h.default_timeout = 3
+        h.await_text("1")
 
