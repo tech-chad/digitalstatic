2a728c216af5899e56b17410b913b8c1d963303a
Chad Larson <techchad2@gmail.com>
2019-10-05 16:53:33 -0500

    Bump version to 0.6

--- dstatic.py
+++ dstatic.py
@@ -7,7 +7,7 @@ import sys
 from random import randint
 from time import sleep
 
-version = "0.5"
+version = "0.6"
 
 curses_number_ch_codes = {48: 0, 49: 1, 50: 2, 51: 3, 52: 4, 53: 5, 54: 6, 55: 7, 56: 8, 57: 9}
 
