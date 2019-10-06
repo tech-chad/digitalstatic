edb30a0ea88727b4f37bedd414077127e2d9c933
Chad Larson <techchad2@gmail.com>
2019-10-05 23:21:04 -0500

    Bumped version to 0.7

--- dstatic.py
+++ dstatic.py
@@ -7,7 +7,7 @@ import sys
 from random import randint, choice
 from time import sleep
 
-version = "0.6"
+version = "0.7"
 
 curses_number_ch_codes = {48: 0, 49: 1, 50: 2, 51: 3, 52: 4, 53: 5, 54: 6, 55: 7, 56: 8, 57: 9}
 color_list = ["red", "green", "blue", "yellow", "magenta", "cyan", "black", "white"]
